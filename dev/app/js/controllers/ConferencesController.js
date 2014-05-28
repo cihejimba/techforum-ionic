ffb38ef22a8c6262a47c6b57bbe6694878306013
maximegens <maximegens@gmail.com>
2014-05-28 15:52:55 +0200

    Bug : back button go back on day 1

--- dev/app/js/controllers/ConferencesController.js
+++ dev/app/js/controllers/ConferencesController.js
@@ -8,8 +8,26 @@ angular.module('app')
 
         $scope.conferences = [];
         $scope.scheduleconferences = [];
-        $scope.numButton = 1;
         $scope.selectedConferenceId = -1;
+
+        if (localStorage.getItem('pageDayConference') !== null){
+            var numPageDay =  localStorage.getItem('pageDayConference');
+            $scope.numButton = numPageDay;
+            $scope.search = {day : numPageDay};
+        }else{
+            $scope.search = {day : 1};
+            $scope.numButton = 1;
+        }
+
+        /**
+         * Change page for day
+         * @param pageDay number of page
+         */
+        $scope.clickPageDay = function(pageDay){
+            localStorage.setItem("pageDayConference", pageDay);
+            $scope.search.day = pageDay;
+            $scope.numButton = pageDay;
+        };
 
         /** Retrieve all conference for to display in conference list
          * Is not conference in localStorage = retrieve by internet and display a loading
